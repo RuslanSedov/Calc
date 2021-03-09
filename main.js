b9ab5d4012b084f13868b60b0b039793028774ad
RuslanSedov <asjr2727@yandex.ru>
2021-03-09 18:37:13 +0300

    Added: Math.sqrt()
    Added the ability to extract the root from an expression

--- main.js
+++ main.js
@@ -9,6 +9,12 @@ function back() {
   let exp = document.form.textview.value
   document.form.textview.value = exp.substring(0, exp.length - 1)
 }
+function radic() {
+  let exp = document.form.textview.value
+  if (exp) {
+    document.form.textview.value = Math.sqrt(exp)
+  }
+}
 function equal() {
   let exp = document.form.textview.value
   if (exp) {
